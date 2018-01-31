9a03efc0041752aa6689f7be5406948cafd4b25c
William O'Sullivan-Dargis <william19.od@gmail.com>
2018-01-31 16:17:28 -0500

    Toute marche pi mnt c clean af

--- PIXEL-TOWER/Assets/Scripts/Player.cs
+++ PIXEL-TOWER/Assets/Scripts/Player.cs
@@ -22,18 +22,24 @@ public class Player : MonoBehaviour {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
 
-
         Move(horizontal, vertical);
     }
-    private void Move(float h,float v) {
+    private void Move(float h, float v)
+    {
 
-        movement.Set(h,v, 0f);
+        movement.Set(h, v, 0f);
         movement = movement.normalized * speed * Time.deltaTime;
-       playerRigidbody.MovePosition(transform.position + movement);
-
+        playerRigidbody.MovePosition(transform.position + movement);
+        if (h + v == 0)
+        {
+            anim.SetBool("IsMoving", false);
+        }
+        else anim.SetBool("IsMoving", true);
     }
 
-    void faceMouse() {
+
+        void faceMouse()
+    {
 
         Vector3 mousePosition = Input.mousePosition;
         mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
@@ -43,39 +49,17 @@ public class Player : MonoBehaviour {
         System.Console.WriteLine(angle);
         System.Console.WriteLine("caca");
         print(angle);
+        anim.SetFloat("DirectionAngle", angle);
 
-        if (angle <= 30)
-        {
-            anim.SetInteger("Movement", 0);
-            transform.localScale = new Vector3(1, 1, 1);
-        }
-        else if (angle > 30 && angle <= 115)
-        {
-            anim.SetInteger("Movement", 1);
 
+        if (angle > 30 && angle <= 160)
+        {
             if (direction.x >= 0)
             {
                 transform.localScale = new Vector3(1, 1, 1);
             }
             else transform.localScale = new Vector3(-1, 1, 1);
-        }
-        else if (angle > 115 && angle <= 160)
-        {
-            anim.SetInteger("Movement", 2);
-            if (direction.x >= 0)
-            {
-                transform.localScale = new Vector3(1, 1, 1);
-            }
-            else transform.localScale = new Vector3(-1, 1, 1);
-        }
-        else if (angle > 160)
-        {
-            anim.SetInteger("Movement", 3);
-            transform.localScale = new Vector3(1, 1, 1);
         }
 
     }
-
-
-
 }
