fb63fb4e8baba9f9d8f4a1bbc92ccf6136be28e5
William O'Sullivan-Dargis <william19.od@gmail.com>
2018-02-01 14:54:01 -0500

    animation ép
    ais

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+
+    private Animator anim;
+    public GameObject player;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //Attack();
+        }
+        faceMouse();
+    }
+
+    void faceMouse()
+    {
+
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        Vector2 direction = new Vector2(mousePosition.x - player.transform.position.x, mousePosition.y - player.transform.position.y);
+
+        transform.up = direction;
+
+    }
+
+}
