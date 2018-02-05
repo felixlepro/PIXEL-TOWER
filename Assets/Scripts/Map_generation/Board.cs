e9838c09d98c36bffb8528ba241b925259fc2a9f
Unknown <felixlepro2006@gmail.com>
2018-02-04 21:25:11 -0500

    Debut scripts

---
+++ Assets/Scripts/Map_generation/Board.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour {
+    public enum TileType
+    {
+        wall,floor
+    }
+    public int hauteur = 100;
+    public int largeur = 100;
+    public IntRange numRooms = new IntRange(15, 20);
+    public IntRange hautRooms = new IntRange(3, 10);
+    public IntRange largRooms = new IntRange(3, 10);
+    public IntRange longCorridor = new IntRange(6, 10);
+    public GameObject[] floortiles;
+    public GameObject mur_Nord;
+    public GameObject mur_Sud;
+    public GameObject mur_Est;
+    public GameObject mur_Ouest;
+    public GameObject voidTile;
+
+    private TileType[][] tiles;
+    private Room[] rooms;
+    private Corridor[] corridor;
+    private GameObject boardHolder;
+
+    private void Start()
+    {
+        boardHolder = new GameObject("Board Holder");
+
+        SetUpTilesArray();
+
+        CreateRoomsCorridor();
+
+
+        
+
+    }   
+    private void SetUpTilesArray()
+    {
+        tiles = new TileType[largeur][];
+        for (int i = 0;i<tiles.Length;i++)
+        {
+            tiles[i] = new TileType[hauteur];
+        }
+    }
+    private void CreateRoomsCorridor()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Map_generation/IntRange.cs
@@ -0,0 +1,16 @@
+﻿using System;
+[Serializable]
+public class IntRange {
+    public int min;
+    public int max;
+
+    public IntRange(int mini, int maxi)
+    {
+        min = mini;
+        max = maxi;
+    }
+    public int Random
+    {
+        get { return UnityEngine.Random.Range(min, max); }
+    }
+}
