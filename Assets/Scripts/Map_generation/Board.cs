a46d9a455890d4308fd7dbbf15ed23bb9d5cd700
Unknown <felixlepro2006@gmail.com>
2018-02-05 14:08:46 -0500

    Minro a tout perdu
    PS: Do not stash/Pop

--- Assets/Scripts/Map_generation/Board.cs
+++ Assets/Scripts/Map_generation/Board.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class Board : MonoBehaviour {
     public enum TileType
     {
-        wall,floor
+        Wall,Floor
     }
     public int hauteur = 100;
     public int largeur = 100;
     public IntRange numRooms = new IntRange(15, 20);
-    public IntRange hautRooms = new IntRange(3, 10);
-    public IntRange largRooms = new IntRange(3, 10);
+    public IntRange hautRoom = new IntRange(3, 10);
+    public IntRange largRoom = new IntRange(3, 10);
     public IntRange longCorridor = new IntRange(6, 10);
     public GameObject[] floortiles;
     public GameObject mur_Nord;
@@ -22,7 +22,7 @@ public class Board : MonoBehaviour {
 
     private TileType[][] tiles;
     private Room[] rooms;
-    private Corridor[] corridor;
+    private Corridor[] corridors;
     private GameObject boardHolder;
 
     private void Start()
@@ -31,10 +31,11 @@ public class Board : MonoBehaviour {
 
         SetUpTilesArray();
 
-        CreateRoomsCorridor();
+        CreateRoomsAndCorridors();
 
 
-        
+
+
 
     }   
     private void SetUpTilesArray()
@@ -45,8 +46,195 @@ public class Board : MonoBehaviour {
             tiles[i] = new TileType[hauteur];
         }
     }
-    private void CreateRoomsCorridor()
+    void CreateRoomsAndCorridors()
     {
+        // Create the rooms array with a random size.
+        rooms = new Room[numRooms.Random];
 
+        // There should be one less corridor than there is rooms.
+        corridors = new Corridor[rooms.Length - 1];
+
+        // Create the first room and corridor.
+        rooms[0] = new Room();
+        corridors[0] = new Corridor();
+
+        // Setup the first room, there is no previous corridor so we do not use one.
+        rooms[0].SetupRoom(largRoom, hautRoom, largeur, hauteur);
+
+        // Setup the first corridor using the first room.
+        corridors[0].SetupCorridor(rooms[0], longCorridor, largRoom, hautRoom, largeur, hauteur, true);
+
+        for (int i = 1; i < rooms.Length; i++)
+        {
+            // Create a room.
+            rooms[i] = new Room();
+
+            // Setup the room based on the previous corridor.
+            rooms[i].SetupRoom(largRoom, hautRoom, largeur, hauteur, corridors[i - 1]);
+
+            // If we haven't reached the end of the corridors array...
+            if (i < corridors.Length)
+            {
+                // ... create a corridor.
+                corridors[i] = new Corridor();
+
+                // Setup the corridor based on the room that was just created.
+                corridors[i].SetupCorridor(rooms[i], longCorridor, largRoom, hautRoom, largeur, hauteur, false);
+            }
+
+            
+        }
+
+    }
+
+
+    void SetTilesValuesForRooms()
+    {
+        // Go through all the rooms...
+        for (int i = 0; i < rooms.Length; i++)
+        {
+            Room currentRoom = rooms[i];
+
+            // ... and for each room go through it's width.
+            for (int j = 0; j < currentRoom.largRoom; j++)
+            {
+                int xCoord = currentRoom.posX + j;
+
+                // For each horizontal tile, go up vertically through the room's height.
+                for (int k = 0; k < currentRoom.hautRoom; k++)
+                {
+                    int yCoord = currentRoom.posY + k;
+
+                    // The coordinates in the jagged array are based on the room's position and it's width and height.
+                    tiles[xCoord][yCoord] = TileType.Floor;
+                }
+            }
+        }
+    }
+
+
+    void SetTilesValuesForCorridors()
+    {
+        // Go through every corridor...
+        for (int i = 0; i < corridors.Length; i++)
+        {
+            Corridor currentCorridor = corridors[i];
+
+            // and go through it's length.
+            for (int j = 0; j < currentCorridor.longCorridor; j++)
+            {
+                // Start the coordinates at the start of the corridor.
+                int xCoord = currentCorridor.startX;
+                int yCoord = currentCorridor.startY;
+
+                // Depending on the direction, add or subtract from the appropriate
+                // coordinate based on how far through the length the loop is.
+                switch (currentCorridor.direction)
+                {
+                    case Direction.Nord:
+                        yCoord += j;
+                        break;
+                    case Direction.Est:
+                        xCoord += j;
+                        break;
+                    case Direction.Sud:
+                        yCoord -= j;
+                        break;
+                    case Direction.Ouest:
+                        xCoord -= j;
+                        break;
+                }
+
+                // Set the tile at these coordinates to Floor.
+                tiles[xCoord][yCoord] = TileType.Floor;
+            }
+        }
+    }
+
+
+    void InstantiateTiles()
+    {
+        // Go through all the tiles in the jagged array...
+        for (int i = 0; i < tiles.Length; i++)
+        {
+            for (int j = 0; j < tiles[i].Length; j++)
+            {
+                // ... and instantiate a floor tile for it.
+                InstantiateFromArray(floorTiles, i, j);
+
+                // If the tile type is Wall...
+                if (tiles[i][j] == TileType.Wall)
+                {
+                    // ... instantiate a wall over the top.
+                    InstantiateFromArray(wallTiles, i, j);
+                }
+            }
+        }
+    }
+
+
+    void InstantiateOuterWalls()
+    {
+        // The outer walls are one unit left, right, up and down from the board.
+        float leftEdgeX = -1f;
+        float rightEdgeX = largeur + 0f;
+        float bottomEdgeY = -1f;
+        float topEdgeY = hauteur + 0f;
+
+        // Instantiate both vertical walls (one on each side).
+        InstantiateVerticalOuterWall(leftEdgeX, bottomEdgeY, topEdgeY);
+        InstantiateVerticalOuterWall(rightEdgeX, bottomEdgeY, topEdgeY);
+
+        // Instantiate both horizontal walls, these are one in left and right from the outer walls.
+        InstantiateHorizontalOuterWall(leftEdgeX + 1f, rightEdgeX - 1f, bottomEdgeY);
+        InstantiateHorizontalOuterWall(leftEdgeX + 1f, rightEdgeX - 1f, topEdgeY);
+    }
+
+
+    void InstantiateVerticalOuterWall(float xCoord, float startingY, float endingY)
+    {
+        // Start the loop at the starting value for Y.
+        float currentY = startingY;
+
+        // While the value for Y is less than the end value...
+        while (currentY <= endingY)
+        {
+            // ... instantiate an outer wall tile at the x coordinate and the current y coordinate.
+            InstantiateFromArray(outerWallTiles, xCoord, currentY);
+
+            currentY++;
+        }
+    }
+
+
+    void InstantiateHorizontalOuterWall(float startingX, float endingX, float yCoord)
+    {
+        // Start the loop at the starting value for X.
+        float currentX = startingX;
+
+        // While the value for X is less than the end value...
+        while (currentX <= endingX)
+        {
+            // ... instantiate an outer wall tile at the y coordinate and the current x coordinate.
+            InstantiateFromArray(outerWallTiles, currentX, yCoord);
+
+            currentX++;
+        }
+    }
+
+
+    void InstantiateFromArray(GameObject[] prefabs, float xCoord, float yCoord)
+    {
+        // Create a random index for the array.
+        int randomIndex = Random.Range(0, prefabs.Length);
+
+        // The position to be instantiated at is based on the coordinates.
+        Vector3 position = new Vector3(xCoord, yCoord, 0f);
+
+        // Create an instance of the prefab from the random index of the array.
+        GameObject tileInstance = Instantiate(prefabs[randomIndex], position, Quaternion.identity) as GameObject;
+
+        // Set the tile's parent to the board holder.
+        tileInstance.transform.parent = boardHolder.transform;
     }
 }
--- Assets/Scripts/Map_generation/Room.cs
+++ Assets/Scripts/Map_generation/Room.cs
@@ -1,8 +1,57 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Room : MonoBehaviour {
+public class Room
+{
+    public int posX;
+    public int posY;
+    public int largRoom;
+    public int hautRoom;
+    public Direction enteringCorridor;
+    
+    public void SetupRoom(IntRange rangeLarg, IntRange  rangeHaut, int columns,int rows)
+    {
+        largRoom = rangeLarg.Random;
+        hautRoom = rangeHaut.Random;
 
-	
+        posX = Mathf.RoundToInt(columns / 2f - largRoom / 2f);
+        posY = Mathf.RoundToInt(rows / 2f - hautRoom / 2f);
+        
+    }
+	public void SetupRoom(IntRange rangeLarg, IntRange rangeHaut, int columns, int rows, Corridor corridor)
+    {
+        enteringCorridor = corridor.direction;
+        largRoom = rangeLarg.Random;
+        hautRoom = rangeHaut.Random;
+        switch (corridor.direction)
+        {
+           case Direction.Nord:
+                hautRoom = Mathf.Clamp(hautRoom, 1, rows - corridor.EndPosY);
+                posY = corridor.EndPosY;
+                posX = Random.Range(corridor.EndPosX - largRoom + 1, corridor.EndPosX);
+
+                posX = Mathf.Clamp(posX, 0, columns - largRoom);
+                break;
+            case Direction.Est:
+                largRoom = Mathf.Clamp(largRoom, 1, columns - corridor.EndPosX);
+                posX = corridor.EndPosX;
+
+                posY = Random.Range(corridor.EndPosY - hautRoom + 1, corridor.EndPosY);
+                posY = Mathf.Clamp(posY, 0, rows - hautRoom);
+                break;
+            case Direction.Sud:
+                hautRoom = Mathf.Clamp(hautRoom, 1, corridor.EndPosY);
+                posY = corridor.EndPosY - hautRoom + 1;
+
+                posX = Random.Range(corridor.EndPosX - largRoom + 1, corridor.EndPosX);
+                posX = Mathf.Clamp(posX, 0, columns - largRoom);
+                break;
+            case Direction.Ouest:
+                largRoom = Mathf.Clamp(largRoom, 1, corridor.EndPosX);
+                posX = corridor.EndPosX - largRoom + 1;
+
+                posY = Random.Range(corridor.EndPosY - hautRoom + 1, corridor.EndPosY);
+                posY = Mathf.Clamp(posY, 0, rows - hautRoom);
+                break;
+        }
+    }
 }
