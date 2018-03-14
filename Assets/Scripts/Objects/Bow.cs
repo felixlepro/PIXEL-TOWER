f50fbddeebc3c637a68331fbcb9aae67d4749f49
William O'Sullivan-Dargis <william19.od@gmail.com>
2018-03-14 13:14:58 -0400

    fuck gtkrakem

--- Assets/Scripts/Objects/Bow.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Weapon", menuName = "Weapon/Bow")]
-public class Bow : Weapon
-{
-    public float boltSpeed;
-}
--- Assets/Scripts/Objects/Sword.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Weapon", menuName = "Weapon/Sword")]
-public class Sword : Weapon
-{
-
-}
