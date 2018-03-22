198ce501cb0f0b4904ab1b03cf80407a5d92a199
Unknown <simon.pad@hotmail.com>
2018-03-22 11:45:13 -0400

    Shop part esti

---
+++ Assets/ButtonImageScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonImageScript : MonoBehaviour {
+
+    public GameObject panel;
+       
+    
+      
+
+
+    private void Update()
+    {
+        
+    }
+
+
+    public void OpenInfo()
+    {
+        panel.SetActive(true);
+    }
+    public void CloseInfo()
+    {
+        panel.SetActive(false);
+    }
+}
